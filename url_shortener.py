197d1b54c2a3842a7a4f694d0ed4805d2d34aa5e
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2019-09-12 21:36:58 -0400

    finally got working

--- url_shortener.py
+++ url_shortener.py
@@ -29,4 +29,4 @@ os.system("git add . > /dev/null 2>&1")
 os.system("git commit -m " + "\"shortening " + url_to_shorten + " to " + shortened_url + " \" > /dev/null 2>&1")
 os.system("git push > /dev/null 2>&1")
 
-print("url has been shortened to:", github_url + "/" + shortened_url)
+print("url has been shortened to:", github_url + "/" + shortened_url + "/")
