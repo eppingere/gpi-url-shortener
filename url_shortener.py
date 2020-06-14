e8474c74099759e98caa5091cadf8495bfc03d4a
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-06-14 19:10:38 -0400

    shortening abc.xyz to https://eppi.ng/gpi-url-shortener/abc/

--- url_shortener.py
+++ url_shortener.py
@@ -10,24 +10,31 @@ print("what url do you want to shorten?")
 url_to_shorten = input("url to be shortened: http(s)://")
 shortened_url = input("shorten to " + github_url + "/")
 
-to_write = shortened_url + "/index.html"
+to_write = path_to_repo + "/" + shortened_url + "/index.html"
 
-if os.path.exists(to_write):
+if os.path.exists(to_write) or os.path.exists(path_to_repo + "/" + shortened_url):
     print("unfortunately you have already shortened something to that url :(")
 
-os.mkdir(shortened_url)
+os.mkdir(path_to_repo + "/" + shortened_url)
 
-index = open(to_write, "w")
+index = open(to_write, "w+")
 
-index.write("<meta http-equiv=\"refresh\" content=\"0; URL=\'http://" + url_to_shorten + "\'\" />")
+index.write("<meta http-equiv=\"refresh\" content=\"0; URL=\'http://" +
+            url_to_shorten + "\'\" />")
 
 index.close()
 
 print("doing some stuff....")
 
+current_directory = os.getcwd()
+os.chdir(path_to_repo)
+
 os.system("git add . > /dev/null 2>&1")
-os.system("git commit -m " + "\"shortening " + url_to_shorten + " to " + github_url + "/" + shortened_url + "/\" > /dev/null 2>&1")
+os.system("git commit -m " + "\"shortening " + url_to_shorten +
+          " to " + github_url + "/" + shortened_url + "/\" > /dev/null 2>&1")
 os.system("git push > /dev/null 2>&1")
+
+os.chdir(current_directory)
 
 print("url has been shortened to:", github_url + "/" + shortened_url + "/")
 print("it might take ~5 mins for the link to become active")
