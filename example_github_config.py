0d440d216d9dc41ecbb6dd6eefd673d6e57649c5
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-10-17 11:37:47 -0400

    shortening google.com to https://eppi.ng/gpi-url-shortener/google/

--- example_github_config.py
+++
@@ -1,8 +0,0 @@
-# if you have a custom domain setup for your url shortener put it here
-github_url = "https://eppi.ng/gpi-url-shortener"
-
-# if you dont, its no problem just use
-# github_url = "https://[your github username].github.io/gpi-url-shortener"
-
-# put the absolute path to the url shortener repo on your
-path_to_repo = "/ABSOLUTE/PATH/TO/gpi-url-shortener"
